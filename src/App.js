1bc9008e081dda56a3fb3d7f1ed5fb9fd1545bdf
Kaustubh Iyer <kaustubhiyer1998@gmail.com>
2021-04-22 21:51:26 +0530

    finished basic logic

--- src/App.js
+++ src/App.js
@@ -1,17 +1,34 @@
 import React from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import { robots } from './robots'
+import { robots } from './robots';
 
-const App = () => {
-    return (
-        <div className="tc">
-            <h1>RoboFriends</h1>
-            <SearchBox />
-            <CardList robots={robots} />
-        </div>
-        
-    );
+
+class App extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            robots,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value });
+    }
+    render = () => {
+        const filteredRobots = this.state.robots.filter(robot => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        });
+        return (
+            <div className="tc">
+                <h1>RoboFriends</h1>
+                <SearchBox onSearchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
+            </div>
+
+        );
+    }
 }
 
-export default App
+export default App;
